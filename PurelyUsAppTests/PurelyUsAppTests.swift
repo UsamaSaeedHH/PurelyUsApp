af0a6b9226120f30f4801a3ce690ed78e8a4f78a
Akbar Khan <akbarkhanios.cdz@gmail.com>
2024-11-30 09:54:56 +0500

    Initial Commit

---
+++ PurelyUsAppTests/PurelyUsAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  PurelyUsAppTests.swift
+//  PurelyUsAppTests
+//
+//  Created by MacBook Pro on 30/11/2024.
+//
+
+import Testing
+@testable import PurelyUsApp
+
+struct PurelyUsAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
